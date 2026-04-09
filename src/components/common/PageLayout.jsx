586512f051e0c7c485e068cfa8f034e20ddec086
Studio9 <99willslv@gmail.com>
2026-04-08 23:33:24 -0300

    Update PageLayout.jsx

--- src/components/common/PageLayout.jsx
+++ src/components/common/PageLayout.jsx
@@ -1,1 +1,7 @@
-
+export default function PageLayout({ children, className = '' }) {
+  return (
+    <main className={`md:ml-64 pt-4 pb-8 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto ${className}`}>
+      {children}
+    </main>
+  )
+}
