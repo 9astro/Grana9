e88bd0f26b9153cd8abd75444d762594ac6ce87d
Studio9 <99willslv@gmail.com>
2026-04-08 23:32:30 -0300

    Update Card.jsx

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -1,1 +1,16 @@
+import { clsx } from 'clsx'
 
+export default function Card({ children, className, hover = true, ...props }) {
+  return (
+    <div
+      className={clsx(
+        'bg-white dark:bg-slate-800 rounded-xl p-6 border border-gray-200 dark:border-slate-700 transition',
+        hover && 'hover:shadow-lg hover:border-gray-300 dark:hover:border-slate-600',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  )
+}
